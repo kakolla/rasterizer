c6ceb6be624a9552d5090b10cd4efb7c7e802203
kakolla <abhishekkakolla1@gmail.com>
2026-09-18 17:58:03 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+#include "image_format.h"
+#include <iostream>
+
+
+
+Color white = {255, 255, 255, 255};
+Color green = {0, 255, 0, 255};
+Color red = {0, 0, 255, 255};
+Color blue = {255, 0, 0, 255};
+
+
+
+int main(int argc, char** argv) {
+	int width = 128;
+	int height = 128;
+	
+
+
+
+
+
+
+
+	return 0;
+}
