3ddc1fe30b32062b8d29109da08a1f32941b2c05
kakolla <abhishekkakolla1@gmail.com>
2026-09-21 14:16:57 -0700

    draw line func

--- main.cpp
+++ main.cpp
@@ -16,6 +16,18 @@ Color red = {0, 0, 255, 255};
 Color blue = {255, 0, 0, 255};
 
 
+void line(int ax, int ay, int bx, int by, Image& framebuffer, Color& color ) {
+	// paint a line by its parametric eq
+	int x_t;
+	int y_t;
+
+	for (float t =0.0 ; t < 1.0; t+=0.02) {
+		x_t = ax + t * (bx - ax);
+		y_t = ay + t * (by - ay);
+		framebuffer.set(x_t, y_t, color);
+	}
+}
+	
 
 int main(int argc, char** argv) {
 	int width = 128;
@@ -27,6 +39,8 @@ int main(int argc, char** argv) {
 	int ax  = 7, ay = 20;
 	int bx  = 17, by = 30;
 	int cx  = 37, cy = 40;
+
+	line(ax, ay, cx, cy, framebuffer, red);
 	
 	framebuffer.set(ax, ay, red);
 	framebuffer.set(bx, by, green);
